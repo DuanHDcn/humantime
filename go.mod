de742a2b3c2771e946ac1560ab40f0f963e7d975
duanhongda <duanhongda@bytedance.com>
2023-11-21 19:24:23 +0800

    feat: convert seconds into human-readable time

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DuanHDcn/humantime
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	secondPtr := flag.Int64("second", 0, "-second=digit")
+	flag.Parse()
+
+	if secondPtr == nil || *secondPtr == 0 {
+		fmt.Println("[Usage] humantime -second=digit")
+		return
+	}
+
+	seconds := *secondPtr
+	second := seconds % 60
+	minute := seconds / 60 % 60
+	hour := seconds / 3600 % 24
+	day := seconds / 3600 / 24
+
+	output := ""
+	output += ternaryString(day > 0, fmt.Sprintf("%dd ", day), "")
+	output += ternaryString(hour > 0, fmt.Sprintf("%dh ", hour), "")
+	output += ternaryString(minute > 0, fmt.Sprintf("%dm ", minute), "")
+	output += ternaryString(second > 0, fmt.Sprintf("%ds ", second), "")
+
+	fmt.Println(output)
+}
+
+func ternaryString(cond bool, t, f string) string {
+	if cond {
+		return t
+	}
+	return f
+}
